f2773361fa6d707f5f247c3ef2fb303bf9bc4db5
eddy <eddy.mwaniki.27@gmail.com>
2025-03-08 23:30:39 +0300

    passes tests

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,3 +8,25 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Game(Base):
+    __tablename__ = "games"
+
+    id = Column(Integer(), primary_key = True )
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    reviews = relationship("Review", backref=("game"))
+
+    def __repr__(self):
+        return f"Game : {games.title} Genre: {games.genre}"
+
+class Review(Base):
+    __tablename__ = "reviews"
+    id = Column(Integer(), primary_key = True)    
+    score = Column(Integer())
+    comment = Column(String())
+    game_id = Column(Integer(), ForeignKey("games.id"))
+    def __repr__(self):
+        return f"Score : {reviews.score} Comment : {reviews.comment}  "
